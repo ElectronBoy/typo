df06a57a6a6f5ff307fcbafbd335d12fbcd680f5
saasbook <saasbook@saasbook.(none)>
2012-12-01 21:27:47 -0800

    Category bugfix

--- app/models/category.rb
+++ app/models/category.rb
@@ -73,6 +73,14 @@ class Category < ActiveRecord::Base
     permalink
   end
 
+
+  def self.get_or_build_category id = nil
+    return Category.find(id) if id
+    category = Category.new.tap do |cat|
+    end
+  end
+
+
   protected
 
   before_save :set_permalink
